2acba6247d2a43d84302666e796ebbacc402e45b
Sili Wen <siliwen@gmail.com>
2025-06-06 15:23:14 -0700

    prod deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,22 @@
 import dotenv from "dotenv";
-dotenv.config();
 
+import serverless from "serverless-http";
 import { app } from "./app";
+
+dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
 
 const port = Number(process.env.PORT);
 
-app(port).catch((err) => {
-  console.error("Failed to start server:", err);
-  process.exit(1);
-});
+if (!isProduction) {
+  app(port).catch((err) => {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  });
+}
+
+// AWS production environment
+const serverlessApp = serverless(app);
+export const handler = async (event: any, context: any) => {
+  return serverlessApp(event, context);
+};
